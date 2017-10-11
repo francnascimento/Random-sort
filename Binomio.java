49c6e1273264cc98f35b9041971247014b3d4353
Gabriel Arruda <gabrielpardi@outlook.com>
2017-10-11 17:04:53 -0300

    Change.

--- Binomio.java
+++ Binomio.java
@@ -8,9 +8,8 @@ class Binomio{
     }
 
 	private static int factorial(int i) {
-        if(i == 0) {
-            return 1;
-        }
+        if(i == 0) return 1;
+        System.out.println(i);
         int product = 1;
         for(int j = 1; j<=i; j++){
             product *= j;
