848f58e8aebd15010a141e5ce035c74d2a1d25d3
Gabriel Arruda <gabrielpardi@outlook.com>
2017-10-11 16:44:41 -0300

    New function.

---
+++ Binomio.java
@@ -0,0 +1,19 @@
+class Binomio{
+    public static void main(String[] args){
+        int n = 8;
+        int i = 8;
+        System.out.println(factorial(n));
+        int coefficient = factorial(n) / (factorial(i) * factorial(n - i));
+        System.out.println(coefficient);
+    }
+
+	private static int factorial(int i) {
+        if(i == 0) return 1;
+        int product = 1;
+        for(int j = 1; j<=i; j++){
+            product *= j;
+        }
+        return product;
+    }
+
+}
